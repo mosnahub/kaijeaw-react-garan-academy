a38db3cfb0262e7f5dde96f1fc2f24334dc82c4a
Tee-SRW <61543306+Tee-SRW@users.noreply.github.com>
2020-04-18 03:35:15 +0700

    Update NavBar.js

--- src/shared/NavBar.js
+++ src/shared/NavBar.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const NavBar = () => (
   <div>
       <div className="logo" />
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
         <Menu.Item key="1"><Link to="/">Home</Link></Menu.Item>
         <Menu.Item key="2"><Link to="/order">Order</Link></Menu.Item>
         <Menu.Item key="3">nav 3</Menu.Item>
