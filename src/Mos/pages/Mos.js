71dceeb3a8ade2c3c23c0d60738e84bf76ae9fa5
mosnahub <punnarat_mos@hotmail.com>
2020-04-25 00:12:33 +0700

    Add card.js, Update Mos.js

--- src/Mos/pages/Mos.js
+++ src/Mos/pages/Mos.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React from "react";
+import { Carousel, Spin, Card  } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
+import "../components/Mos.css";
+
+function onChange(a, b, c) {
+  console.log(a, b, c);
+}
+
+const antIcon = (
+  <LoadingOutlined
+    style={{
+      fontSize: 24,
+    }}
+    spin
+  />
+);
 
 const Mos = () => {
-    return (
+  return (
     <div>
-        <h1>Mos</h1>
+      <h1> Mos </h1>{" "}
+      <Carousel afterChange={onChange}>
+        <div>
+          <h3> 1 </h3>{" "}
+        </div>{" "}
+        <div>
+          <h3> 2 </h3>{" "}
+        </div>{" "}
+        <div>
+          <h3> 3 </h3>{" "}
+        </div>{" "}
+        <div>
+          <h3> 4 </h3>{" "}
+        </div>{" "}
+      </Carousel>{" "}
+      <Spin indicator={antIcon} />{" "}
     </div>
-    );
-}
+  );
+};
 
 export default Mos;
