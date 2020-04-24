ca272a6884a5c366ca8dd0312cb3f3491bf1171e
mosnahub <punnarat_mos@hotmail.com>
2020-04-24 21:51:08 +0700

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -21,14 +21,14 @@ const Routes = () => {
                 <Route exact path="/" component={Home} />
                 <Route exact path="/order" component={Order} />
                 <Route exact path="/mos" component={Mos} />
-                <Route exact path="/oat" component={Oat} />
+                {/* <Route exact path="/oat" component={Oat} />
                 <Route exact path="/jop" component={Jop} />
                 <Route exact path="/zee" component={Zee} />
                 <Route exact path="/p" component={P} />
                 <Route exact path="/name" component={Name} />
                 <Route exact path="/roger" component={Roger} />
                 <Route exact path="/tee" component={Tee} />
-                <Route exact path="/game" component={Game} />
+                <Route exact path="/game" component={Game} /> */}
             </Switch>
         </Router>
     );
