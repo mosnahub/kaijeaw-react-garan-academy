06531be2304db9e907d7a4673a2c87c0e494187d
mosnahub <punnarat_mos@hotmail.com>
2020-04-24 22:22:28 +0700

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -11,7 +11,7 @@ import P from "./P/pages/P"
 import Name from "./Name/pages/Name"
 import Roger from "./Roger/pages/Roger"
 import Tee from "./Tee/pages/Tee"
-// import Game from "./Game/pages/Game"
+import Game from "./Game/pages/Game"
 
 const Routes = () => {
     return (
@@ -28,7 +28,7 @@ const Routes = () => {
                 <Route exact path="/name" component={Name} />
                 <Route exact path="/roger" component={Roger} />
                 <Route exact path="/tee" component={Tee} />
-                {/* <Route exact path="/game" component={Game} /> */}
+                <Route exact path="/game" component={Game} />
             </Switch>
         </Router>
     );
