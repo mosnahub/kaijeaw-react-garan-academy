1a2d32300e5cfef0ed685ec07c5edc7de981e800
thirawat69 <thirawatp9@gmail.com>
2020-05-05 00:50:54 +0700

    Crad

--- src/Zee/pages/Zee.js
+++ src/Zee/pages/Zee.js
@@ -1,13 +1,85 @@
-import React from 'react';
-import { Rate } from 'antd';
+import React from "react";
+import { Rate, Row, Col } from "antd";
+import FoodCards from "../components/FoodCard";
 
+const foodMenu = [
+  {
+    name: "Steak",
+    price: "$39.99",
+    imageUrl:
+      "https://www.thesteakhouseco.com/wp-content/uploads/2018/07/banner-blog-03.jpg",
+  },
+  {
+    name: "Steak",
+    price: "$39.99",
+    imageUrl:
+      "https://www.thesteakhouseco.com/wp-content/uploads/2018/07/banner-blog-03.jpg",
+  },
+  {
+    name: "Steak",
+    price: "$39.99",
+    imageUrl:
+      "https://www.thesteakhouseco.com/wp-content/uploads/2018/07/banner-blog-03.jpg",
+  },
+  {
+    name: "Steak",
+    price: "$39.99",
+    imageUrl:
+      "https://www.thesteakhouseco.com/wp-content/uploads/2018/07/banner-blog-03.jpg",
+  },
+  {
+    name: "Steak",
+    price: "$39.99",
+    imageUrl:
+      "https://www.thesteakhouseco.com/wp-content/uploads/2018/07/banner-blog-03.jpg",
+  },
+  {
+    name: "Steak",
+    price: "$39.99",
+    imageUrl:
+      "https://www.thesteakhouseco.com/wp-content/uploads/2018/07/banner-blog-03.jpg",
+  },
+  {
+    name: "Steak",
+    price: "$39.99",
+    imageUrl:
+      "https://www.thesteakhouseco.com/wp-content/uploads/2018/07/banner-blog-03.jpg",
+  },
+  {
+    name: "Steak",
+    price: "$39.99",
+    imageUrl:
+      "https://www.thesteakhouseco.com/wp-content/uploads/2018/07/banner-blog-03.jpg",
+  },
+  {
+    name: "Steak",
+    price: "$39.99",
+    imageUrl:
+      "https://www.thesteakhouseco.com/wp-content/uploads/2018/07/banner-blog-03.jpg",
+  },
+  {
+    name: "Steak",
+    price: "$39.99",
+    imageUrl:
+      "https://www.thesteakhouseco.com/wp-content/uploads/2018/07/banner-blog-03.jpg",
+  },
+];
 
 const Zee = () => {
-    return (
-        <div>
-            <Rate />
-        </div>
-    );
-}
+  return (
+    <div>
+      {console.log({ foodMenu })}
 
-export default  Zee;
+      <Rate />
+      <Row gutter={[16, 16]}>
+        {foodMenu.map((item, index) => (
+          <Col xs={24} sm={12} md={6} lg={6} xl={6} key={index}>
+            <FoodCards foodItem={item} />
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+};
+
+export default Zee;
